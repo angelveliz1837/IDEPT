5e926f23388bfeb979dd1bd466f0fd46ee8e5807
angelveliz1837 <99308699+angelveliz1837@users.noreply.github.com>
2023-01-13 11:15:10 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$('section.awSlider .carousel').carousel({
+/*$('section.awSlider .carousel').carousel({
 	pause: "hover",
   interval: 2000
 });
@@ -9,8 +9,44 @@ $('section.awSlider').append('<img src="' + startImage + '">');
 $('section.awSlider .carousel').on('slid.bs.carousel', function () {
  var bscn = $(this).find('.item.active > img').attr('src');
 	$('section.awSlider > img').attr('src',bscn);
-});
+});*/
 
+
+//INICIO DE CARROUSEL
+//FUNCION CARRUSEL
+/*var c=1;
+function carrusel() {
+    document.querySelector("#banner__imgcab").style.opacity="0";
+    document.querySelector("#banner__imgcab").style.transition="all 1s";
+    setTimeout("cambio()",1000);
+}
+
+function cambio() {
+    c++;
+    if (c>3) {
+        c=1;
+    }
+    document.querySelector('#banner__imgcab').setAttribute('src','img/banners/imagen_banner'+c+'.jpg');
+    document.querySelector('#banner__imgcab').style.opacity="1";
+    document.querySelector('#banner__imgcab').style.transition="all 1s";
+    setTimeout("carrusel()",2000);
+}
+//FIN CARROUSEL
+
+//FUNCION REDIMENSIONAR FLECHA
+function  redimensionar_flecha() {
+    var derecha= document.getElementById('banner__der');
+    var img= document.getElementById('banner__imgcab');
+
+    var ubica=img.clientWidth-derecha.clientWidth;
+    derecha.style.left=ubica+"px";
+}
+
+//FIN REDIMENSIONAR FLECHA
+//LLAMADO A LA SFUNCIONES PARA SU EJECUCION (aqui tambien cargo la funcion cargarDefectoAviso)
+document.body.setAttribute("onload","carrusel();redimensionar_flecha();fecha()");
+//FIN CARRUSEL *******************************************************************************************
+*/
 
 /* 
 Philips ambilight tv
