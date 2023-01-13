a8815b245e33b8bbcb6dcac07d929729250ea151
angelveliz1837 <99308699+angelveliz1837@users.noreply.github.com>
2023-01-13 11:14:35 -0500

    Update codigo.js

--- js/codigo.js
+++ js/codigo.js
@@ -1,3 +1,41 @@
+//INICIO DE CARROUSEL
+//FUNCION CARRUSEL
+var c=1;
+function carrusel() {
+    document.querySelector("#banner__imgcab").style.opacity="0";
+    document.querySelector("#banner__imgcab").style.transition="all 1s";
+    setTimeout("cambio()",1000);
+}
+
+function cambio() {
+    c++;
+    if (c>3) {
+        c=1;
+    }
+    document.querySelector('#banner__imgcab').setAttribute('src','img/banners/imagen_banner'+c+'.jpg');
+    document.querySelector('#banner__imgcab').style.opacity="1";
+    document.querySelector('#banner__imgcab').style.transition="all 1s";
+    setTimeout("carrusel()",3000);
+}
+//FIN CARROUSEL
+
+//FUNCION REDIMENSIONAR FLECHA
+/*function  redimensionar_flecha() {
+    var derecha= document.getElementById('banner__der');
+    var img= document.getElementById('banner__imgcab');
+
+    var ubica=img.clientWidth-derecha.clientWidth;
+    derecha.style.left=ubica+"px";
+}*/
+
+//FIN REDIMENSIONAR FLECHA
+//LLAMADO A LA SFUNCIONES PARA SU EJECUCION (aqui tambien cargo la funcion cargarDefectoAviso)
+document.body.setAttribute("onload","carrusel();fecha()");
+//FIN CARRUSEL *******************************************************************************************
+
+
+//****************CALENDARIO******************/
+
 var dsemanas = new Array("Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab");
 //                         0      1      2      3      4      5      6
 var meses = new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto",
@@ -64,7 +102,8 @@ function fecha(){
 }
 
 //fecha(); //llamada a la funcion fecha
-document.body.setAttribute("onload","fecha();");
+//document.body.setAttribute("onload","fecha();");
+//document.body.setAttribute("onload","carrusel();redimensionar_flecha();fecha()");
 
 /*--------REDES DE CONTACTANOS--------------------------*/
 function gira(e){
